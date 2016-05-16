c317ca5ea017105ad2eafe803b0c6a223ff565e6
Idris Hanafi <ifh101@gmail.com>
2016-05-16 00:38:59 -0400

    Edit

--- PHPFiles/DB_Config.php
+++ PHPFiles/DB_Config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
  
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', "lolidris"); // db password (mention your db password here)
+define('DB_USER', ""); // db user
+define('DB_PASSWORD', ""); // db password (mention your db password here)
 define('DB_DATABASE', "user_info"); // database name
 define('DB_SERVER', "127.0.0.1"); // db server
 
